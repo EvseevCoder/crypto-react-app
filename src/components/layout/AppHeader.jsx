29980f817d6a5331820ee918c3457342b3e40ac4
IGOR EVSSEV <andrey.semin.evseev@gmail.com>
2024-03-18 22:45:30 +0300

    create Header (18.03.24)

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -1,14 +1,95 @@
-import { Layout } from "antd";
+import { Layout, Select, Space, Button, Modal, Drawer } from "antd";
+import CryptoContext from "../../context/crypto-context";
+import { useContext, useEffect, useState } from "react";
+import CoininfoModal from "../CoinInfoModal";
+import AddAssetForm from "../AddAssetForm";
 
 const headerStyle = {
+  with: "100%",
   textAlign: "center",
-  color: "#fff",
   height: 60,
-  paddingInline: 48,
   lineHeight: "64px",
-  backgroundColor: "#4096ff",
+  padding: "1rem",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  // background: "white",
 };
 
 export default function AppHeader() {
-  return <Layout.Header style={headerStyle}>Header</Layout.Header>;
+  const { crypto } = useCrypto();
+  const [select, setSelect] = useState(false);
+  const [coin, setCoin] = useState(null);
+  const [drawer, setDrawer] = useState(null);
+  const [modal, setModal] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("keypress", keypress);
+    return () => document.removeEventListener("keypress", keypress);
+  }, []);
+
+  function keypress(event) {
+    if (event.key == "/") {
+      setSelect((prev) => !prev);
+    }
+  }
+
+  const handleSelect = (value) => {
+    setCoin(crypto.find((c) => c.id == value));
+    console.log(`selected ${value}`);
+    setModal(true);
+  };
+
+  return (
+    <Layout.Header style={headerStyle}>
+      <Select
+        open={select}
+        onSelect={handleSelect}
+        onClick={() => setSelect((prev) => !prev)}
+        value={"press / to open"}
+        style={{ width: "250px" }}
+        defaultValue={["china"]}
+        options={crypto.map((coin) => ({
+          label: coin.name,
+          value: coin.id,
+          icon: coin.icon,
+        }))}
+        optionRender={(option) => (
+          <Space>
+            <img
+              style={{ width: "20px" }}
+              src={option.data.icon}
+              alt={option.data.label}
+            />{" "}
+            {option.data.label}
+          </Space>
+        )}
+      />
+      <Button
+        type="primary"
+        onClick={() => {
+          setDrawer(true);
+        }}
+      >
+        Add Asset
+      </Button>
+
+      <Modal
+        footer={null}
+        open={modal}
+        onOk={() => setModal(false)}
+        onCancel={() => setModal(false)}
+      >
+        <CoininfoModal coin={coin} />
+      </Modal>
+
+      <Drawer title="Add Asset" onClose={() => setDrawer(false)} open={drawer}>
+        <AddAssetForm />
+      </Drawer>
+    </Layout.Header>
+  );
+}
+
+export function useCrypto() {
+  return useContext(CryptoContext);
 }
--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,8 +1,9 @@
 import { Layout, Card, Statistic, List, Typography, Spin, Tag } from "antd";
 import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
-import { useEffect, useState } from "react";
-import { FetchAssets, FetchCrypto } from "../../api";
-import { percentDif, capitalize } from "./utils";
+import { useContext, useEffect, useState } from "react";
+
+import { percentDif, capitalize } from "../../utils";
+import CryptoContext from "../../context/crypto-context";
 
 const siderStyle = {
   padding: "1rem",
@@ -17,38 +18,7 @@ const data = [
 ];
 
 export default function AppSider() {
-  const [loading, setLoading] = useState(false);
-  const [crypto, setCrypto] = useState([]);
-  const [assets, setAssets] = useState([]);
-
-  useEffect(() => {
-    async function preload() {
-      setLoading(true);
-      const { result } = await FetchCrypto();
-      const assets = await FetchAssets();
-
-      setAssets(
-        assets.map((asset) => {
-          const coin = result.find((c) => c.id === asset.id);
-
-          return {
-            grow: asset.price < coin.price,
-            growPercent: percentDif(asset.price, coin.price),
-            totalAmount: asset.amount * coin.price,
-            totalProfit: asset.amount * coin.price - asset.amount * asset.price,
-            ...asset,
-          };
-        })
-      );
-      setCrypto(result);
-      setLoading(false);
-    }
-    preload();
-  }, []);
-
-  if (loading) {
-    return <Spin fullscreen />;
-  }
+  const { loading, assets, crypto } = useContext(CryptoContext);
 
   return (
     <Layout.Sider width="25%" style={siderStyle}>
